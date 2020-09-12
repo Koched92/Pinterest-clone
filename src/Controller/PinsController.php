599b8213ffc164928a1c4fa24b5d4f54d90fa035
Koched92 <hichem.koched@gmail.com>
2020-09-12 15:33:02 +0200

    token_csrf

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
+
     /**
      * @Route("/")
      */
-    public function index(): Response
+    public function index(PinRepository $repo): Response
     {
-        return $this->render('pins/index.html.twig');
+
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
+
+    /**
+     * @Route("/create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em){
+
+        if($request->isMethod('POST')){
+            $data = $request->request->all();
+
+            if($this->isCsrfTokenValid('pins_create', $data['_token'])){
+                $pin = new Pin();
+                $pin->setTitle($data['title']);
+                $pin->setDescription($data['description']);
+
+                $em->persist($pin);
+                $em->flush();
+            }
+
+            return $this->redirect('/');
+
+        }
+        return $this-> render('pins/create.html.twig');
+    }
+
+
+
 }
